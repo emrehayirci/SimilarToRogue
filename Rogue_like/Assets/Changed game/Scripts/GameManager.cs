db8e40de559e33defddc7668554c1d65360d6e6b
Emre Hayırcı <emrehayirci@gmail.com>
2018-05-17 13:49:16 +0200

    Resettin Branch Last

---
+++ Rogue_like/Assets/Changed game/Scripts/GameManager.cs
@@ -0,0 +1,122 @@
+﻿using Assets.Changed_game.Scripts.Level;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelStartDelay = 0f;
+    public static GameManager instance = null;
+    public float turnDelay = .1f;
+    public NewBoardManager boardScript;
+
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playerTurn = true;
+
+    public Text levelText;
+    public GameObject levelImage;
+    private int level = 1;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup;
+
+	// Use this for initialization
+	void Awake () {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        
+
+        enemies = new List<Enemy>();
+        boardScript = GetComponent<NewBoardManager>();
+        InitGame();
+	}
+
+    void InitGame()
+    {
+        doingSetup = true;
+
+        levelImage = GameObject.Find("Level Image");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+
+        ShowLevelImageAfterLoad();
+        Invoke("HideLevelImage",levelStartDelay);
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+
+    public void ShowLevelImageAfterLoad()
+    {
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+    }
+
+    public void ShowLevelImageBeforeLoad(Text newlevelText, GameObject newLevelImage)
+    {
+        newlevelText.text = "Day " + (level + 1);
+        newLevelImage.SetActive(true);
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if(playerTurn || enemiesMoving || doingSetup)
+        {
+            return;
+        }
+        StartCoroutine(MoveEnemies());
+	}
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playerTurn = true;
+        enemiesMoving = false;
+    }
+    
+    void OnLevelWasLoaded(int index)
+    {
+        level++;
+        InitGame();
+    }
+}
---
+++ Rogue_like/Assets/Changed game/Scripts/Level/PathFinding.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using Priority_Queue;
+using UnityEngine;
+
+namespace Assets.Changed_game.Scripts.Level
+{
+    class PathFinding
+    {
+        public static Dictionary<Vector2, Vector2> cameFrom;
+        public static Dictionary<Vector2, double> costSoFar;
+
+        /*
+            Heuristic function for A* pathfinding. Simply prioritize movements that will be done.
+            Calculates Heuristical cost of movement
+            A* is a path finding algorithm prioritize the direction (heuristic for our case)
+         */
+        static public double Heuristic(Vector2 a, Vector2 b)
+        {
+            return Math.Abs(a.x - b.x) + Math.Abs(a.y - b.y);
+        }
+
+        /* 
+            Implements A* path finding algorithm (Proved to be best solution of that kind of problems)
+            PARAMS: 
+                Takes tiles arrray that board manager probably have. 
+                startx and starty coordinates for starting position on the map
+                endx and endy coordinates for ending position
+        */
+        public static Stack<Vector2> Calculate(Vector2 start, Vector2 end)
+        {
+            //Get tiles from BoardManager
+            TileType[,] tiles = GameManager.instance.boardScript.BoardTiles;
+
+            cameFrom = new Dictionary<Vector2, Vector2>();
+            costSoFar = new Dictionary<Vector2, double>();
+            var frontier = new FastPriorityQueue<Location>(100);
+            frontier.Enqueue(new Location(start), 0);
+
+            cameFrom[start] = start;
+            costSoFar[start] = 0;
+
+            while (frontier.Count > 0)
+            {
+                var current = frontier.Dequeue().position;
+
+                if (current.Equals(end))
+                {
+                    break;
+                }
+
+                //get neighbours
+                List<Vector2> neighbours = new List<Vector2>();
+                if(current.x < tiles.GetLength(0) -1 ){ //first dimension
+                    neighbours.Add(new Vector2(current.x + 1, current.y));
+                }
+
+                if(current.y < tiles.GetLength(1) - 1 ){ //sec dimension
+                    neighbours.Add(new Vector2(current.x, current.y + 1));
+                }
+
+                if(current.x > 0 ){
+                    neighbours.Add(new Vector2(current.x - 1, current.y));
+                }
+
+                if(current.y > 0 ){
+                    neighbours.Add(new Vector2(current.x, current.y - 1));
+                }
+
+                foreach (var next in neighbours)
+                {
+                    double newCost = costSoFar[current];
+                    
+                    newCost = costSoFar[current] + ((tiles[(int)next.x, (int)next.y] == TileType.Wall) ? 3 : 1);
+
+                    if (!costSoFar.ContainsKey(next)
+                        || newCost < costSoFar[next])
+                    {
+                        costSoFar[next] = newCost;
+                        double priority = newCost + Heuristic(next, end);
+                        frontier.Enqueue(new Location(next), (float)priority);
+                        cameFrom[next] = current;
+                    }
+                }
+            }
+            //Debug.Log(costSoFar);
+            //Debug.Log(cameFrom);
+            return BackTrace(start, end);
+        }        
+
+        private static Stack<Vector2> BackTrace(Vector2 start, Vector2 end)
+        {
+            Stack<Vector2> moves = new Stack<Vector2>();
+            if (cameFrom.ContainsKey(end))
+            {
+                var current = end;
+                while (current != start)
+                {
+                    //Debug.Log(current);
+                    //Debug.Log(costSoFar[current]);
+                    moves.Push(current);
+                    current = cameFrom[current];
+                }
+            }
+            else
+            {
+                Debug.Log("No way Bro!");
+            }
+            return moves;
+           
+        }
+
+    }
+
+    class Location: FastPriorityQueueNode
+    {
+        public Vector2 position { get; set; }
+
+        public Location(Vector2 position)
+        {
+            this.position = position;
+        }
+    }
+}
